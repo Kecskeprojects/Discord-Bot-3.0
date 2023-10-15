4554fcff0ab69c30d687032cc3a54e218ed99bee
kirschadam <kirsch.a.p@gmail.com>
2023-10-15 23:39:00 +0200

    Update IAdminCommands.cs

--- Discord Bot GUI/Interfaces/Commands/IAdminCommands.cs
+++ Discord Bot GUI/Interfaces/Commands/IAdminCommands.cs
@@ -1,9 +1,15 @@
-﻿using System.Threading.Tasks;
+﻿using Discord.Commands;
+using System.Threading.Tasks;
 
 namespace Discord_Bot.Interfaces.Commands
 {
     public interface IAdminCommands
     {
         Task Help();
+        Task ChannelAdd(string type, [Remainder] string channelName);
+        Task ChannelRemove(string type, [Remainder] string channelName = "all");
+        Task TwitchAdd(string type, [Remainder] string name);
+        Task TwitchRemove(string type, [Remainder] string name = "all");
+        Task ServerSettings();
     }
 }
