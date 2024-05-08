efed8d9b2144956b1d2220664a77a039350dc623
Kecskeprojects <kirsch.a.p@gmail.com>
2024-05-08 14:51:27 +0200

    organized communication

--- Discord Bot GUI/Communication/Log.cs
+++ Discord Bot GUI/Communication/Log.cs
@@ -6,9 +6,7 @@ namespace Discord_Bot.Communication
     public class Log(DateTime time, LogType type, string completeLog)
     {
         public DateTime TimeStamp { get; private set; } = time;
-
         public LogType Type { get; private set; } = type;
-
         public string Content { get; set; } = completeLog;
     }
 }
--- Discord Bot GUI/Communication/MusicRequest.cs
+++ Discord Bot GUI/Communication/MusicRequest.cs
@@ -3,13 +3,9 @@
     public class MusicRequest(string uRL, string title, string thumbnail, string duration, string user)
     {
         public string URL { get; set; } = uRL;
-
         public string Title { get; set; } = title;
-
         public string Thumbnail { get; set; } = thumbnail;
-
         public string Duration { get; set; } = duration;
-
         public string User { get; set; } = user;
     }
 }
